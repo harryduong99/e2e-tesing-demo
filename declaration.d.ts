e45b123090b566186ab2d4ab37912ce683017880
harry-nutsfi <harry@nuts.finance>
2024-06-19 11:09:28 +0700

    fix: setup metamask and CI

---
+++ declaration.d.ts
@@ -0,0 +1,4 @@
+declare module "@synthetixio/synpress/commands/metamask";
+declare module "@synthetixio/synpress/helpers";
+declare module "@synthetixio/synpress/commands/playwright";
+declare module "@synthetixio/synpress/commands/synpress";
