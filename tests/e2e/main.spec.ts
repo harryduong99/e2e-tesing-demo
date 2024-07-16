38877c484a5b7d57ee4089146588abd2c90f206d
harryduong99 <duongminhnam99@gmail.com>
2024-07-16 16:56:34 +0700

    fix: add monocart report

---
+++ tests/e2e/main.spec.ts
@@ -0,0 +1,53 @@
+import { Page } from "@playwright/test";
+import {
+  testSharedState as test,
+  expectSharedState as expect,
+} from "./fixtures";
+import { addCoverageReport } from "monocart-reporter";
+
+let sharedPage: Page;
+
+test.beforeAll(async ({ page }) => {
+  sharedPage = page;
+  const isChromium = test.info().project.name === "chromium";
+  if (isChromium) {
+    await Promise.all([
+      sharedPage.coverage.startJSCoverage({
+        resetOnNavigation: false,
+      }),
+      sharedPage.coverage.startCSSCoverage({
+        resetOnNavigation: false,
+      }),
+    ]);
+  }
+
+  await sharedPage.goto("/");
+});
+
+test.afterAll(async () => {
+  const isChromium = test.info().project.name === "chromium";
+  if (isChromium) {
+    const [jsCoverage, cssCoverage] = await Promise.all([
+      sharedPage.coverage.stopJSCoverage(),
+      sharedPage.coverage.stopCSSCoverage(),
+    ]);
+    const coverageList = [...jsCoverage, ...cssCoverage];
+
+    await addCoverageReport(coverageList, test.info());
+  }
+
+  await sharedPage.close();
+});
+
+test("has title", async () => {
+  await expect(sharedPage).toHaveTitle(/E2E tests/);
+});
+
+test("has title 2", async () => {
+  await expect(sharedPage).toHaveTitle(/E2E tests/);
+});
+
+test("has env value", async () => {
+  const envCheck = await sharedPage.locator(".env-check");
+  await expect(envCheck).toHaveText("env test value");
+});
