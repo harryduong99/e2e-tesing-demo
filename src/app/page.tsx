c6b0d425fc66004eca784030b8923b17fc0fb25d
harryduong99 <duongminhnam99@gmail.com>
2024-09-03 03:13:14 +0700

    chore: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <main>
       <div className="env-check">{process.env.NEXT_PUBLIC_ENV_TEST}</div>
       <div className="env-secrets">{process.env.SIGNING_PRIVATE_KEY}</div>
-      <div></div>
     </main>
   );
 }
