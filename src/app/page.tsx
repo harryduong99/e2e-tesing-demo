21131d7fbecf400573b9b672116002f5d838f735
harryduong99 <duongminhnam99@gmail.com>
2024-09-03 03:11:51 +0700

    fix: update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main>
       <div className="env-check">{process.env.NEXT_PUBLIC_ENV_TEST}</div>
       <div className="env-secrets">{process.env.SIGNING_PRIVATE_KEY}</div>
+      <div></div>
     </main>
   );
 }
